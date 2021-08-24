de038fcb4fee4713d57883b811dd6d791d32bfc4
Tom Bremer <tom@tbremer.com>
2021-08-24 17:19:26 -0500

    initial  push

---
+++ README.md
@@ -0,0 +1,29 @@
+# hyper-prefers-color-scheme
+
+## Concept
+
+Allow for two color schemes for Light and Dark mode when your OS switches themes Hyper adapts as well.
+
+## Install
+
+Add `hyper-prefers-color-scheme` to the plugins array in your ~/.hyper.js config, or run `hyper -i hyper-prefers-color-scheme`.
+
+## Confgure
+
+In your `.hyper.js` config you will need to add a new field `hyperPrefersColorScheme` with two keys `light` & `dark`. Those keys will need to be color schemes that you have installed in your `plugins` array.
+
+### Example `.hyper.js`
+
+```
+{
+  hyperPrefersColorScheme: {
+    light: "hyper-papercolor",
+    dark: "hyper-oceans16"
+  },
+  plugins: [
+    "hyper-papercolor",
+    "hyper-oceans16"
+  ]
+}
+```
+
---
+++ index.js
@@ -0,0 +1,111 @@
+const electron = require('electron');
+
+const hasCalled = {
+  getDefinedThemes: false,
+  isDarkMode: null,
+};
+
+function getThemeObj(themePath, config) {
+  const themeModule = require(themePath);
+  const theme = themeModule.decorateConfig(config);
+
+  return theme;
+}
+
+const getDefinedThemes = (() => {
+  let themes = null;
+  return (pluginsWithPaths, config) => {
+    if (themes) return themes;
+    const { hyperPreferColorScheme } = config;
+    const pathMap = pluginsWithPaths.plugins.reduce(
+      (map, cur) => {
+        if (cur.endsWith(hyperPreferColorScheme.light)) {
+          map.light = cur;
+        }
+        if (cur.endsWith(hyperPreferColorScheme.dark)) {
+          map.dark = cur;
+        }
+
+        return map;
+      },
+      { light: null, dark: null }
+    );
+
+    const lightTheme = getThemeObj(pathMap.light, config);
+    const darkTheme = getThemeObj(pathMap.dark, config);
+
+    themes = { darkTheme, lightTheme };
+
+    hasCalled.getDefinedThemes = true;
+
+    return themes;
+  };
+})();
+
+module.exports = {
+  middleware: (store) => (next) => (action) => {
+    if (window.themes && window.hasSetTheme === false) {
+      const mql = window.matchMedia('(prefers-color-scheme: dark)');
+      window.hasSetTheme = true;
+
+      mql.addEventListener('change', (evt) => {
+        store.dispatch({
+          type: 'PREFERS_COLOR_SCHEME_CHANGE',
+          isDarkMode: evt.matches,
+        });
+      });
+
+      store.dispatch({
+        type: 'PREFERS_COLOR_SCHEME_CHANGE',
+        isDarkMode: mql.matches,
+      });
+    }
+
+    return next(action);
+  },
+
+  onWindow(window) {
+    const themes = getDefinedThemes(electron.app.plugins.getPaths(), electron.app.config.getConfig());
+
+    window.webContents.executeJavaScript(`window.themes=${JSON.stringify(themes)};window.hasSetTheme=false`);
+  },
+
+  reduceUI(state, action) {
+    if (action.type === 'PREFERS_COLOR_SCHEME_CHANGE') {
+      console.log('PREFERS_COLOR_SCHEME_CHANGE', action);
+      const theme = window.themes[action.isDarkMode ? 'darkTheme' : 'lightTheme'];
+      console.table({
+        foregroundColor: theme?.foregroundColor ?? config.foregroundColor,
+        backgroundColor: theme?.backgroundColor ?? config.backgroundColor,
+        borderColor: theme?.borderColor ?? config.borderColor,
+        colors: theme?.colors ?? config.colors,
+        cursorColor: theme?.cursorColor ?? config.cursorColor,
+        selectionColor: theme?.selectionColor ?? config.selectionColor,
+        fontSize: theme?.fontSize ?? config.fontSize,
+        fontFamily: theme?.fontFamily ?? config.fontFamily,
+        css: theme?.css ?? config.css,
+        termCSS: theme?.termCSS ?? config.termCSS,
+        cursorShape: theme?.cursorShape ?? config.cursorShape,
+      });
+
+      return (
+        state
+          //colors
+          .set('foregroundColor', theme?.foregroundColor ?? config.foregroundColor)
+          .set('backgroundColor', theme?.backgroundColor ?? config.backgroundColor)
+          .set('borderColor', theme?.borderColor ?? config.borderColor)
+          .set('colors', theme?.colors ?? config.colors)
+          .set('cursorColor', theme?.cursorColor ?? config.cursorColor)
+          .set('selectionColor', theme?.selectionColor ?? config.selectionColor)
+          //fonts
+          .set('fontSize', theme?.fontSize ?? config.fontSize)
+          .set('fontFamily', theme?.fontFamily ?? config.fontFamily)
+          //misc
+          .set('css', theme?.css ?? config.css)
+          .set('termCSS', theme?.termCSS ?? config.termCSS)
+          .set('cursorShape', theme?.cursorShape ?? config.cursorShape)
+      );
+    }
+    return state;
+  },
+};
